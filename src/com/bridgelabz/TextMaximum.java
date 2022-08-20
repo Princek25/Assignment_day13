61313924a79b8b4474c8005c62ee105d000bd5e0
Prince <princek2530@gmail.com>
2022-08-20 17:47:26 +0530

    Added: Maximum number among 3 float values

--- src/com/bridgelabz/TextMaximum.java
+++ src/com/bridgelabz/TextMaximum.java
@@ -2,13 +2,13 @@ package com.bridgelabz;
 
 public class TextMaximum {
     public static void main(String[] args) {
-        int a = 200;
-        int b = 300;
-        int c = 100;
-        int temp;
-        int temp1;
+        float a = 200.25f;
+        float b = 100.25f;
+        float c = 300.65f;
+        float temp;
+        float temp1;
         temp = a > b ? a:b;
         temp1 = temp >c ? temp:c;
-        System.out.println(temp1);
+        System.out.println("Maximum number among 3 float number: " + temp1);
     }
 }
