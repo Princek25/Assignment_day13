99dcd59ddc65457987294193e8e12a0fe6f19a71
Prince <princek2530@gmail.com>
2022-08-20 17:35:19 +0530

    Added: Maximum Number at 3rd Place

--- src/com/bridgelabz/TextMaximum.java
+++ src/com/bridgelabz/TextMaximum.java
@@ -2,9 +2,9 @@ package com.bridgelabz;
 //Text Maximum number among 3 integer
 public class TextMaximum {
     public static void main(String[] args) {
-        int a = 200; // 300, 200, 100
-        int b = 300;
-        int c = 100;
+        int a = 200;
+        int b = 100;
+        int c = 300;
         int temp;
         int temp1;
         temp = a > b ? a:b;
