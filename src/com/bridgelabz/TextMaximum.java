3f82ca2409b7db73f4f712b140a6ae685f71bbb4
Prince <princek2530@gmail.com>
2022-08-20 18:14:04 +0530

    Added: Maximum String Value

--- src/com/bridgelabz/TextMaximum.java
+++ src/com/bridgelabz/TextMaximum.java
@@ -2,13 +2,18 @@ package com.bridgelabz;
 
 public class TextMaximum {
     public static void main(String[] args) {
-        float a = 200.25f;
-        float b = 100.25f;
-        float c = 300.65f;
-        float temp;
-        float temp1;
-        temp = a > b ? a:b;
-        temp1 = temp >c ? temp:c;
-        System.out.println("Maximum number among 3 float number: " + temp1);
+        String a = "Peach";
+        String b = "Apple";
+        String c = "Banana";
+        String max;
+
+        max=a;
+        if(b.compareTo(max)>0){
+            max=b;
+        }
+        if (c.compareTo(max)>0){
+            max=c;
+        }
+        System.out.println("Maximum String Value among 'Peach', 'Apple, 'Banana' is: " + max);
     }
 }
